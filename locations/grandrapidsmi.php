d90d4f81db3cc4923711bebb30a047ad07a5f257
Pete Hoffswell <pete@hoffswell.com>
2016-06-29 08:20:50 -0400

    link updates
    Venue info update, other link updates.
    Thanks!

--- locations/grandrapidsmi.php
+++ locations/grandrapidsmi.php
@@ -6,13 +6,13 @@ $location = "Grand Rapids, MI";
 $promoHash = "#AlexaGRR";
 $bgsplah = "/img/grandrapidsmi.jpg";
 $datetime = "Oct 1 at 9:00 AM";
-$venuename = "TBD";
+$venuename = "Davenport University";
 $venuemaplink = "https://www.google.com/maps/place/Grand+Rapids,+MI/@42.9563371,-85.7301286,12z/data=!3m1!4b1!4m5!3m4!1s0x88185460bb502815:0xa593aacb1bd3a8d0!8m2!3d42.9633599!4d-85.6680863";
 $eventbritelink = "https://www.eventbrite.com/e/hackster-amazon-present-alexa-skill-building-101-tickets-26064255891";
 $venuesplash = "";
-$venueimg = "/img/tbd.jpg";
+$venueimg = "/img/davenportu.jpg";
 $venuelink = $eventbritelink; // "http://www.randolphschool.net/";
-$venueembed = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d93441.31693599187!2d-85.7301286076295!3d42.95633714998021!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88185460bb502815%3A0xa593aacb1bd3a8d0!2sGrand+Rapids%2C+MI!5e0!3m2!1sen!2sus!4v1467083948101";
+$venueembed = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11700.361700054898!2d-85.5301864!3d42.8498238!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x679c199b80d83215!2sDavenport+University!5e0!3m2!1sen!2sus!4v1467202404409";
 $venueslogan = "( More info coming soon )";
 
 $asktovolunteerlink = "https://www.hackster.io/kayakpete";

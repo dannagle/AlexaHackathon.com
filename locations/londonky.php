4b7cf9c8f9b3028b4e8e6c921221d076ade0b1fb
Johnie Karr <johnie.karr@hotmail.com>
2016-07-03 16:08:06 -0400

    Updating london KY information
    Added location and swapped Host/Co-Host

--- locations/londonky.php
+++ locations/londonky.php
@@ -6,27 +6,26 @@ $location = "London, KY";
 $promoHash = "#AlexaKLOZ";
 $bgsplah = "/img/londonky.jpg";
 $datetime = "Jul 23, Noon";
-$venuename = "TBD";
-$venuemaplink = "https://www.google.com/maps/place/London,+KY/@37.1160043,-84.1161289,13z/data=!3m1!4b1!4m5!3m4!1s0x885ccd149736c703:0x621951e47307cc98!8m2!3d37.1289771!4d-84.0832646";
+$venuename = "Parkway Christian Fellowship";
+$venuemaplink = "https://www.google.com/maps/place/255+Bill+George+Rd,+London,+KY+40741/@37.136988,-84.0632054,17z/data=!4m13!1m7!3m6!1s0x885ccd27c6001d4f:0xe98df3addf13076c!2s255+Bill+George+Rd,+London,+KY+40741!3b1!8m2!3d37.1369837!4d-84.0610167!3m4!1s0x885ccd27c6001d4f:0xe98df3addf13076c!8m2!3d37.1369837!4d-84.0610167";
 $eventbritelink = "https://www.eventbrite.com/e/hackster-amazon-present-alexa-skill-building-101-tickets-26048512803";
 $venuesplash = "";
 $venueimg = "/img/tbd.jpg";
 $venuelink = $eventbritelink; // "http://www.randolphschool.net/";
 $venueembed = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d50904.68150170436!2d-84.11612893992736!3d37.116004250362955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x885ccd149736c703%3A0x621951e47307cc98!2sLondon%2C+KY!5e0!3m2!1sen!2sus!4v1467078592565";
-$venueslogan = "( More info coming soon )";
+$venueslogan = "";
 
 $asktovolunteerlink = "https://twitter.com/dotNetPrgmr";
 
-$hostName = "Johnie Karr";
-$hostLink = "https://twitter.com/dotNetPrgmr";
-$hostImage = "https://pbs.twimg.com/profile_images/660146964932116480/It5NP17J.jpg";
-$hostTitle = "Host";
+$hostName = "Phillip Horn";
+$hostLink = "https://www.hackster.io/hornp";
+$hostImage = "https://hackster.imgix.net/uploads/avatar/file/140541/PhillipHorn-CROP.JPG?w=400&h=400&fit=min";
+$hostTitle = "host";
 
-
-$coHostName = "Phillip Horn";
-$coHostLink = "https://www.hackster.io/hornp";
-$coHostImage = "https://hackster.imgix.net/uploads/avatar/file/140541/PhillipHorn-CROP.JPG?w=400&h=400&fit=min";
-$coHostTitle = "Co-host";
+$coHostName = "Johnie Karr";
+$coHostLink = "https://twitter.com/dotNetPrgmr";
+$coHostImage = "https://pbs.twimg.com/profile_images/660146964932116480/It5NP17J.jpg";
+$coHostTitle = "Co-Host";
 
 $volunteer1Name = "";
 $volunteer1Link = "";

7e68a986dafff61e0e9139d9db7bca015b5ba03d
Dan Nagle <dan@dannagle.com>
2016-07-03 16:41:47 -0500

    added more venue info

--- locations/londonky.php
+++ locations/londonky.php
@@ -10,9 +10,9 @@ $venuename = "Parkway Christian Fellowship";
 $venuemaplink = "https://www.google.com/maps/place/255+Bill+George+Rd,+London,+KY+40741/@37.136988,-84.0632054,17z/data=!4m13!1m7!3m6!1s0x885ccd27c6001d4f:0xe98df3addf13076c!2s255+Bill+George+Rd,+London,+KY+40741!3b1!8m2!3d37.1369837!4d-84.0610167!3m4!1s0x885ccd27c6001d4f:0xe98df3addf13076c!8m2!3d37.1369837!4d-84.0610167";
 $eventbritelink = "https://www.eventbrite.com/e/hackster-amazon-present-alexa-skill-building-101-tickets-26048512803";
 $venuesplash = "";
-$venueimg = "/img/tbd.jpg";
-$venuelink = $eventbritelink; // "http://www.randolphschool.net/";
-$venueembed = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d50904.68150170436!2d-84.11612893992736!3d37.116004250362955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x885ccd149736c703%3A0x621951e47307cc98!2sLondon%2C+KY!5e0!3m2!1sen!2sus!4v1467078592565";
+$venueimg = "/img/parkwaychristianfellowship.png";
+$venuelink = "http://www.parkwaychristianfellowship.com/"; // "http://www.randolphschool.net/";
+$venueembed = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12727.481663741592!2d-84.0609831!3d37.1368422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7b354ddb5fb78175!2sParkway+Christian+Fellowship!5e0!3m2!1sen!2sus!4v1467582017091";
 $venueslogan = "";
 
 $asktovolunteerlink = "https://twitter.com/dotNetPrgmr";

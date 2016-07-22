7da75be4f8640f5b881239f54c042c50b1a4ed4b
Dan Nagle <dan@dannagle.com>
2016-07-21 20:17:40 -0500

    venue update for huntsville

--- locations/huntsvilleal.php
+++ locations/huntsvilleal.php
@@ -7,13 +7,12 @@ $promoHash = "#AlexaHSV";
 $bgsplah = "/img/bigspringpark.jpg";
 $datetime = "Oct 16";
 $datetime = "Oct 16, Noon";
-$venuename = "Randolph School";
-$venuemaplink = "https://www.google.com/maps/place/Randolph+School/@34.7066529,-86.5580531,15z/data=!4m5!3m4!1s0x0:0xbce7e92696f84874!8m2!3d34.7066529!4d-86.5580531";
-$eventbritelink = "https://www.eventbrite.com/e/hackster-amazon-present-alexa-skill-building-101-tickets-26048773583";
-$venuesplash = "/img/randolphmarketing.jpg";
-$venueimg = "/img/RandolphLogoHorz.jpg";
+$venuename = "Randolph School - Garth Campus";
+$venuemaplink = "https://www.google.com/maps/place/Randolph+School+-+Garth+Campus/@34.6958688,-86.5515813,15z/data=!4m5!3m4!1s0x0:0x1b16fc8a3828891b!8m2!3d34.6958688!4d-86.5515813";
+$venuesplash = "/img/randolphblue.png";
+$venueimg = "/img/randolphschool-white800.png";
 $venuelink = "http://www.randolphschool.net/";
-$venueembed = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13119.766400766144!2d-86.5580531!3d34.7066529!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xbce7e92696f84874!2sRandolph+School!5e0!3m2!1sen!2sus!4v1466948683173";
+$venueembed = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13121.476470220248!2d-86.5515813!3d34.6958688!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1b16fc8a3828891b!2sRandolph+School+-+Garth+Campus!5e0!3m2!1sen!2sus!4v1469149104790";
 $venueslogan = "The K-12 independent school in Huntsville, Alabama";
 
 $asktovolunteerlink = "https://dannagle.com/contact";

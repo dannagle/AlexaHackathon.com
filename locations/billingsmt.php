25f364fdbeb41dc11e069678aae7fe53eef1e56f
Dan Nagle <dan@dannagle.com>
2016-08-10 19:09:49 -0500

    updated billings, MT

--- locations/billingsmt.php
+++ locations/billingsmt.php
@@ -5,17 +5,17 @@ $active = true;
 $location = "Billings, MT";
 $promoHash = "#AlexaBIL";
 $bgsplah = "/img/billingsmt.jpg";
-$datetime = "Aug 6, Noon";
-$datetimesimple = "2016-8-6 12:00pm";
-$venuename = "TBD";
+$datetime = "Sept 24, Noon";
+$datetimesimple = "2016-9-24 12:00pm";
+$venuename = "Magic City Makerspace";
 $facebookAlbum = "https://www.facebook.com/media/set/?set=oa.284307005259839&type=3";
-$venuemaplink = "https://www.google.com/maps/place/Billings,+MT/@45.7818339,-108.7037677,11z/data=!3m1!4b1!4m5!3m4!1s0x53486f8888fa9d97:0x373556d4f179b550!8m2!3d45.7832856!4d-108.5006904";
+$venuemaplink = "https://www.google.com/maps/place/21+S+29th+St,+Billings,+MT+59101/@45.7790784,-108.5045835,17z/data=!4m5!3m4!1s0x53487b317be41cb7:0xae5c9fff5645aa45!8m2!3d45.779271!4d-108.504177";
 $eventbritelink = "https://www.eventbrite.com/e/hackster-amazon-present-alexa-skill-building-101-tickets-26046995264";
 $venuesplash = "";
-$venueimg = "/img/tbd.jpg";
-$venuelink = $eventbritelink; // "http://www.randolphschool.net/";
-$venueembed = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d178078.00686665968!2d-108.7037677430768!3d45.7818338588772!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53486f8888fa9d97%3A0x373556d4f179b550!2sBillings%2C+MT!5e0!3m2!1sen!2sus!4v1467082047008";
-$venueslogan = "( More info coming soon )";
+$venueimg = "/img/createreuse.png";
+$venuelink = "http://scrapinbillings.com/magic-city-makerspace/"; // "http://www.randolphschool.net/";
+$venueembed = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2782.60636964281!2d-108.5045835171448!3d45.77907842291214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53487b317be41cb7%3A0xae5c9fff5645aa45!2s21+S+29th+St%2C+Billings%2C+MT+59101!5e0!3m2!1sen!2sus!4v1470873643370";
+$venueslogan = "Magic City Makerspace";
 
 $asktovolunteerlink = "https://www.hackster.io/jreese";
 
